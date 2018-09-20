b96548f38c0372df9116ace2e4a9f287c38e6529
rasool <rasools27@gmail.com>
2018-09-20 09:40:37 +0530

    initial commit

---
+++ DoWhileExample/src/main/java/DoWhileExample.java
@@ -0,0 +1,14 @@
+public class DoWhileExample {
+ 
+  public static void main(String[] args) {
+    int i =0;   
+    
+    do
+    {
+      System.out.println("i is : " + i);
+      i++;
+      
+    }while(i < 5);
+    
+  }
+}
